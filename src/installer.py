77d3d76e7bfa0417a5e96d79567789dd4de2e8e5
Fr0sne <gabriel16david@hotmail.com>
2022-02-14 23:12:00 -0300

    Delete installer.py

--- src/installer.py
+++
@@ -1,8 +0,0 @@
-import os
-
-with open("libs.txt", "r") as f:
-     for line in f.readlines():
-          lib = line.split()[0]
-          print(f"Instalando {lib}")
-          print(f"pip {lib}")
-          os.system(f"pip install {lib}")
